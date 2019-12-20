4432f1ae807810ffb5e01a87693e0953147758a2
김세한 <kmp1007s@naver.com>
2019-12-20 15:05:00 +0900

    Commit before yarn eject
    Commit before yarn eject

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,54 +1,22 @@
 import React from "react";
+import styles from "./Navbar.module.scss";
+import { FaSearch } from "react-icons/fa";
 
 const Navbar = () => {
   return (
     <>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          width: "100%",
-          padding: "35px",
-          boxShadow:
-            "0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)"
-        }}
-      >
-        <span
-          style={{
-            marginRight: 40,
-            color: "blue"
-          }}
-        >
-          도란도란
-        </span>
-        <span
-          style={{
-            marginRight: 20
-          }}
-        >
-          팀 생성
-        </span>
-        <span
-          style={{
-            marginRight: 20
-          }}
-        >
-          마이페이지
-        </span>
-        <span
-          style={{
-            marginRight: 20
-          }}
-        >
-          로그인
-        </span>
-        <span
-          style={{
-            marginRight: 20
-          }}
-        >
-          회원가입
-        </span>
+      <div className={styles.container}>
+        <span className={styles.logo}>도란도란</span>
+        <div className={styles.inputArea}>
+          <FaSearch
+            style={{ verticalAlign: "middle", color: "gray", marginRight: 10 }}
+          />
+          <input type="text" placeholder="검색" className={styles.input} />
+        </div>
+        <span>팀 생성</span>
+        <span>마이페이지</span>
+        <span>로그인</span>
+        <span>회원가입</span>
       </div>
     </>
   );
