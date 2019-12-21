e6692835a4eb7a80717e83560286917af5397d63
김세한 <kmp1007s@naver.com>
2019-12-21 10:50:45 +0900

    Add Navbar Component
    Add Navbar Component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Link } from "react-router-dom";
 import styles from "style/Navbar.module.scss";
+import { IconContext } from "react-icons";
 import { FaSearch } from "react-icons/fa";
 
 const Navbar = () => {
+  const inputArea = useRef(null);
+  const input = useRef(null);
+
+  const handleInputFocus = () => {
+    inputArea.current.classList.toggle(styles.input_focus_on);
+
+    for (let el of inputArea.current.children)
+      el.classList.toggle(styles.input_focus_on);
+  };
+
   return (
     <>
       <div className={styles.container}>
         <span className={styles.logo}>도란도란</span>
-        <div className={styles.inputArea}>
-          <FaSearch
-            style={{ verticalAlign: "middle", color: "gray", marginRight: 10 }}
+        <div className={styles.input_area} ref={inputArea}>
+          <IconContext.Provider value={{ className: styles.search_icon }}>
+            <FaSearch />
+          </IconContext.Provider>
+          <input
+            type="text"
+            placeholder="검색"
+            className={styles.input}
+            onFocus={handleInputFocus}
+            onBlur={handleInputFocus}
+            ref={input}
           />
-          <input type="text" placeholder="검색" className={styles.input} />
         </div>
-        <Link to="/">팀 생성</Link>
+        <Link to="/">그룹 생성</Link>
         <Link to="/">마이페이지</Link>
         <Link to="/">로그인</Link>
         <Link to="/">회원가입</Link>
