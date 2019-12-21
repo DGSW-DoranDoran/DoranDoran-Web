c43c4ada30afb03b4eb26a08a4f01fc7f9a28c18
jaem1n207 <jaem1n207@naver.com>
2019-12-21 14:45:03 +0900

    [ADD] RegistrationContainer

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { Switch, Route /* Redirect */ } from "react-router-dom";
 import TestPage from "pages/TestPage";
+import Navbar from "./Navbar/Navbar";
 import LoginPage from "../pages/LoginPage";
+import TeamRegistraionPage from "../pages/TeamRegistraionPage";
 
 const App = () => {
   return (
     <Switch>
       <Route exact path={"/"} render={() => <TestPage />} />
       <Route exact path={"/login"} render={() => <LoginPage />} />
+      <Route exact path={"/navbar"} render={() => <Navbar />} />
+      <Route
+        exact
+        path={"/TeamRegistraionPage"}
+        render={() => <TeamRegistraionPage />}
+      />
     </Switch>
   );
 };
---
+++ src/pages/TeamRegistraionPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import TeamRegistraionContainer from "../containers/TeamRegistrationContainer/TeamRegistrationContainer";
+
+const TeamRegistraionPage = () => {
+  return <TeamRegistraionContainer />;
+};
+
+export default TeamRegistraionPage;
