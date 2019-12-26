5758271462050b76a5ae3a11ca1a5b435c61f33d
jaem1n207 <jaem1n207@naver.com>
2019-12-26 18:42:42 +0900

    [ADD] LoginFetch

--- src/stores/login/loginRepository.js
+++ src/stores/login/loginRepository.js
@@ -6,31 +6,33 @@ class loginRepository {
     let response = null;
     let status = null;
     await axios
-      .post(`http://10.80.161.54:3001/auth/login/`, {
+      .post(`http://10.80.162.235:3001/auth/login/`, {
         id: username,
         password
       })
       .then(({ data }) => {
         response = data;
         status = data.status;
-        console.log(status);
-
+        console.log(response);
         // eslint-disable-next-line default-case
         switch (status) {
+          case 200:
+            return 1;
           case 400:
             return 0;
           case 403:
             return 0;
           case 409:
             return 0;
+          default:
+            return 0;
         }
-        console.log(response);
       })
       .catch(err => {
         console.log(err);
         return 0;
       });
-    return response;
+    return response, status;
   };
 }
 
