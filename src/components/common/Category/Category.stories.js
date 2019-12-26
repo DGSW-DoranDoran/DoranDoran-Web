b9d903991fa8c6711252aceba77cc9d01f41bbe3
김세한 <kmp1007s@naver.com>
2019-12-26 10:23:58 +0900

    UPDATE MakeTeam Style
    UPDATE MakeTeam Style

--- src/components/common/Category/Category.stories.js
+++ src/components/common/Category/Category.stories.js
@@ -6,5 +6,8 @@ export default {
 };
 
 export const category = () => {
-  return <Category />;
+  const CATEGORY = ["대회", "식사", "게임", "프로젝트", "기타"];
+  const curCategory = CATEGORY[0];
+
+  return <Category categories={CATEGORY} currentCategory={curCategory} />;
 };
